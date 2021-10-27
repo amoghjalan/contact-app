38f0c88f622113fa8443109354071ab3581a9619
amoghjalan <amoghjalan2005@gmail.com>
2021-10-27 23:11:47 +0530

    Added Authentication Added CRUD funtionality for Contacts

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
-
+const connectDB = require("./config/db");
 const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
 
 app.get("/", (req, res) => {
   res.json({ msg: "Contact App" });
