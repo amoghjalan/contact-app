d54a785bc0716c1a9a79e08d101c53872517e548
amoghjalan <amoghjalan2005@gmail.com>
2021-10-29 01:24:59 +0530

    Fixed Production Bugs

--- server.js
+++ server.js
@@ -21,4 +21,6 @@ if (process.env.NODE_ENV === "production") {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on post ${PORT}`));
+app.listen(PORT, "0.0.0.0", () =>
+  console.log(`Server started on post ${PORT}`)
+);
