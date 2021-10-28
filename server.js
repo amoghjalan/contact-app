303b9cba3a00fabec63c2c6d119d316c7fb2f33f
amoghjalan <amoghjalan2005@gmail.com>
2021-10-28 21:41:11 +0530

    Setting up production env

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 const app = express();
 
 connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Contact App" });
-});
-
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/contacts", require("./routes/contacts"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
